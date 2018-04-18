582fde2fd8fb65936b56446343cac5abb87da88d
Alain Kaiser <alain.kaiser1@students.fhnw.ch>
2018-04-18 10:56:00 +0200

    reviewed round keys functions

--- SPN/SPN.CTR.Framework/Spn.cs
+++ SPN/SPN.CTR.Framework/Spn.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SPN.CTR.Framework
 {
-    class Spn : IMode
+    public class Spn : IMode
     {
         public int R { get; private set; }
         public int N { get; private set; }
@@ -18,7 +18,8 @@ namespace SPN.CTR.Framework
         public int RoundKeyPosition { get; private set; }
         public Dictionary<string, string> SBox { get; private set; }
         public Dictionary<int, int> Bitpermutation { get; private set; }
-        private Dictionary<int, string> _roundKeys;
+        private string[] _roundKeys;
+        private string[] _roundKeysInverse;
 
         public Spn(int r, int n, int m, int s, string key, int roundKeyLength, int roundKeyPosition,
             Dictionary<string, string> sBox, Dictionary<int, int> bitpermutation)
@@ -32,22 +33,17 @@ namespace SPN.CTR.Framework
             RoundKeyPosition = roundKeyPosition;
             SBox = sBox ?? throw new ArgumentNullException(nameof(sBox));
             Bitpermutation = bitpermutation;
-            _roundKeys = new Dictionary<int, string>();
 
-            // Calculate Round Keys
-            CalculateRoundKeys();
-        }
-
-        private void CalculateRoundKeys()
-        {
-            for (int i = 0; i <= R; i++)
-            {
-                _roundKeys.Add(i, Key.Substring((RoundKeyPosition * i), RoundKeyLength));
-            }
+            // Initialisiere Objekte
+            _roundKeys = new string[R + 1];
+            _roundKeysInverse = new string[R + 1];
         }
 
         public string Encrypt(string textToEncrypt)
         {
+            // Calculate Round Keys
+            CalculateRoundKeys();
+
             // Initial Step
             string x = Helper.XorStrings(textToEncrypt, _roundKeys[0]);
 
@@ -71,7 +67,47 @@ namespace SPN.CTR.Framework
 
         public string Decrypt(string text)
         {
-            throw new NotImplementedException();
+            // Berechnung der initialen Rundenschlüssel
+            CalculateRoundKeys();
+
+            // Berechnung der inversen Rundenschlüssel
+            CalculateRoundKeysInverse();
+
+
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// Berechnet die initialen Rundenschlüssel gemäss SPN und Definition der Rundenschlüsselfunktion
+        /// </summary>
+        private void CalculateRoundKeys()
+        {
+            _roundKeys = new string[R + 1];
+
+            for (int i = 0; i <= R; i++)
+            {
+                _roundKeys[i] = Key.Substring((RoundKeyPosition * i), RoundKeyLength);
+            }
+        }
+
+        /// <summary>
+        /// Berechnet die Rundenschlüssel für die SPN Entschlüsselung
+        /// Dabei wird der 0te und der Rte Rundenschlüssel einfach an der Position vertauscht
+        /// Alle restlichen Rundenschlüssel tauschen auch die Position (von aussen nach innen), werden jedoch noch Bitpermutiert
+        /// </summary>
+        private void CalculateRoundKeysInverse()
+        {
+            for (int i = 0; i < _roundKeys.Length; i++)
+            {
+                if (i != 0 && i != _roundKeys.Length - 1)
+                {
+                    _roundKeysInverse[i] = Helper.Bitpermutation(_roundKeys[_roundKeys.Length - (i + 1)], Bitpermutation);
+                }
+                else
+                {
+                    _roundKeysInverse[i] = _roundKeys[_roundKeys.Length - (i+1)];
+                }
+            }
         }
     }
 }
