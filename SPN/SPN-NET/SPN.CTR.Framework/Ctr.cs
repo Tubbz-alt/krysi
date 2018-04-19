2d9f7066dc9aa6bf67bf6d689168da1251977a4f
Alain Kaiser <alain.kaiser1@students.fhnw.ch>
2018-04-19 11:28:07 +0200

    added comments to Ctr class

--- SPN/SPN-NET/SPN.CTR.Framework/Ctr.cs
+++ SPN/SPN-NET/SPN.CTR.Framework/Ctr.cs
@@ -23,28 +23,37 @@ namespace SPN.CTR.Framework
             _spn = spn ?? throw new ArgumentNullException(nameof(spn));
         }
 
+        /// <summary>
+        /// Verschlüsselung von einem Text im Ctr Modus mit Hilfe eines SPN's
+        /// </summary>
+        /// <param name="text">Text, welcher verschlüsselt werden soll</param>
+        /// <returns>Verschlüsselter Text als String</returns>
         public string Encrypt(string text)
         {
-            string stringArray = CtrHelper.PrepareStringForCtr(text, L);
+            // String hübsch machen, für CTR
+            string ctrString = CtrHelper.PrepareStringForCtr(text, L);
 
-            // For testing purposes, use "0000010011010010".
-            String yMinusOne = CtrHelper.RandomNumberForCtrMode(16);
+            // Generierung von einem zufällen Bit-String der Länge L --> y[-1]
+            String yMinusOne = CtrHelper.RandomNumberForCtrMode(L);
 
-            // Ausgangs-String in Teile aufteilen
-            string[] parts = stringArray.SplitInParts(L).ToArray();
+            // ctrString in Teile der Länge L aufteilen
+            string[] parts = ctrString.SplitInParts(L).ToArray();
+
+            // Erstellung Resultat-Liste und hinzufügen von y[n-1]
             List<string> resultList = new List<string>();
             resultList.Add(yMinusOne);
 
+            // Ctr Steps
             for (int i = 0; i < parts.Length; i++)
             {
-                // (y-1) + 0 + 1 + n-1
+                // y[-1] + 0 + 1 + n-1
                 int yResult = Convert.ToInt32((Convert.ToInt32(yMinusOne, 2) + i) % Math.Pow(2, L));
                 string binaryStringYResult = Convert.ToString(yResult, 2).PadLeft(16, '0');
 
-                // Entschlüsselung vom binaryStringYResult mit dem SPN und dem Key
+                // Verschlüsselungs vom binaryStringYResult mit dem SPN und dem Key
                 string spnResult = _spn.Decrypt(binaryStringYResult, false);
 
-                // XOR spnResult mit y0, y1, yn-1 values
+                // XOR spnResult mit x0, x1, xn-1 values
                 string xi = spnResult.Xor(parts[i]);
                 resultList.Add(xi);
             }
@@ -52,18 +61,25 @@ namespace SPN.CTR.Framework
             return string.Join(String.Empty, resultList);
         }
 
+        /// <summary>
+        /// Entschlüsselung von einem Text im Ctr Modus mit Hilfe eines SPN's
+        /// </summary>
+        /// <param name="text">Text, welcher entschlüsselt werden soll</param>
+        /// <param name="isDecryptAfterEncrypt">Boolean welcher angibt, ob nach Verschlüsselung entschlüsselt wird</param>
+        /// <returns>Entschlüsselter Text als String</returns>
         public string Decrypt(string text, bool isDecryptAfterEncrypt)
         {
-            List<string> resultList = new List<string>();
-
             // Zerlege y in Blöcke der Länge L
             List<string> parts = text.SplitInParts(L).ToList();
+
+            // Resultat-Liste erstellen
+            List<string> resultList = new List<string>();
+
+            // y[-1] holen
             string yMinusOne = parts[0];
 
             // Entferne y[n-1] von der Liste
             parts.RemoveAt(0);
-
-            //resultList.Add(yMinusOne);
 
             for (int i = 0; i < parts.Count; i++)
             {
